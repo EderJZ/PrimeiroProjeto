4bb05cf32e97a32812c548f3f497200638e99aed
EderZ <ederzanardinidk8159@gmail.com>
2024-01-17 16:39:35 -0300

    ajuste email

--- enviar-email.php
+++ enviar-email.php
@@ -1,31 +1,35 @@
 <?php
-  //Variáveis
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $mensagem = $_POST['mensagem'];
-  $data_envio = date('d/m/Y');
-  $hora_envio = date('H:i:s');
+header('Content-Type: application/json');
 
-  //Compo E-mail
-  $arquivo = "
+// Recebe os dados do formulário
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['mensagem'];
+
+// Compo E-mail
+$arquivo = "
     <html>
       <p><b>Nome: </b>$nome</p>
       <p><b>E-mail: </b>$email</p>
       <p><b>Mensagem: </b>$mensagem</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
     </html>
-  ";
-  
-  //Emails para quem será enviado o formulário
-  $destino = "ederzanardinii@gmail.com";
-  $assunto = "Contato pelo Site";
+";
 
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+// E-mails para quem será enviado o formulário
+$destino = "ederzanardinii@gmail.com";
+$assunto = "Contato pelo Site";
 
-  //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
-  
-  echo "<meta http-equiv='refresh' content='10;URL=./index.html'>";
+// Cabeçalhos
+$headers  = "MIME-Version: 1.0\n";
+$headers .= "Content-type: text/html; charset=utf-8\n";
+$headers .= "From: $nome <$email>";
+
+// Enviar e verificar se o e-mail foi enviado com sucesso
+if (mail($destino, $assunto, $arquivo, $headers)) {
+    $response = array('status' => 'success', 'message' => 'E-mail enviado com sucesso.');
+} else {
+    $response = array('status' => 'error', 'message' => 'Erro ao enviar o e-mail.');
+}
+
+echo json_encode($response);
+?>
